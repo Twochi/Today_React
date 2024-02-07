c9d4056fda9f2b2bcd6c9a54aa2ad4e338f1988f
DESKTOP-7VKL7O0\82102 <ghdrldnjs0213@gmail.com>
2024-02-07 14:41:04 +0900

    giwonCommit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import style from "./Login.module.css"
+
+function Login() {
+
+    console.log("완냐")
+
+    return(
+    <>
+
+    <div className={style.mainContainer}>
+        <div className={style.loginForm}>
+            <div>회원가입</div>
+        </div>
+    </div>
+
+    </>
+    
+    )
+}
+
+export default Login;
