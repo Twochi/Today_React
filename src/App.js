b12affa87675b70321843d5faabded6e5bdf6cb0
DESKTOP-7VKL7O0\82102 <ghdrldnjs0213@gmail.com>
2024-02-07 14:52:18 +0900

    giwonCommit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import Login from "./pages/login/Login";
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}>
-
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
