38e1da3f9b0c57327d234bf117a875770d6f1dd1
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2024-02-12 20:46:53 +0900

    jisu note 페이지 틀 완성

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,16 @@
 
 import logo from "../images/logo.png"
 import style from "./Header.module.css"
+import { useNavigate} from "react-router-dom";
 
 function Header() {
 
+    const navigate = useNavigate();
+
+    // logo 클릭시 메인페이지로 이동하는 함수 (임시 ==> 로그인 페이지)
+    const onClickHome = () =>{
+        navigate('/');
+    }
     return (
 
         <>
--- src/layouts/Note.js
+++ src/layouts/Note.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Note.css";
 import notebookImage from "../images/Notebook.png";
-
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 function Note({ children }) {
   return (
     <div className="note-container">
       <img src={notebookImage} alt="Notebook" className="note-image" />
-      <div> {children} </div>
+        <div className="note-header">
+            오늘의 하루
+        </div>
+        <div className="note-weather">
+            (오늘의 날씨)
+        </div>
+        <div className="note-todoList">
+            TodoList
+        </div>
+        <div className="note-calendar">
+            <Calendar/>
+        </div>
     </div>
   );
 }
