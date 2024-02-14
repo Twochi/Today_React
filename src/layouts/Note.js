9dba91457f7e4736c7c7302502c92a44c2a59f45
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2024-02-14 15:32:30 +0900

    커밋

--- src/layouts/Note.js
+++ src/layouts/Note.js
@@ -2,23 +2,13 @@ import React from "react";
 import "./Note.css";
 import notebookImage from "../images/Notebook.png";
 
-<<<<<<< HEAD
-function Note({ children }) {
-    return (
-        <div className="note-container">
-            <img src={notebookImage} alt="Notebook" className="note-image" />
-            <div> {children} </div>
-        </div>
-    );
-=======
 function Note() {
   return (
     <div className="note-container">
       <img src={notebookImage} alt="Notebook" className="note-image" />
-      
+
     </div>
   );
->>>>>>> giwon
 }
 
 export default Note;
