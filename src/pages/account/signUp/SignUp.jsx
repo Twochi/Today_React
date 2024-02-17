d9a6e9f697fcc5e8673925b226b85521e71efbf2
DESKTOP-7VKL7O0\82102 <ghdrldnjs0213@gmail.com>
2024-02-18 01:37:48 +0900

    giwonCommit

--- src/pages/account/signUp/SignUp.jsx
+++ src/pages/account/signUp/SignUp.jsx
@@ -1,31 +1,181 @@
+// SignUp.js
+
+import React, { useState, useRef } from 'react';
 import "../../../layouts/Default.css";
 import Note from "../../../layouts/Note";
 import style from "./SignUp.module.css"
 import images from "../../../images/logo.png"
+import baseImg from "../../../images/baseImg.jpg"
+import downArrow from "../../../images/downArrowImg.png"
+
 
 function SignUp() {
+    const [gender, setGender] = useState('');
+    const [customEmail, setCustomEmail] = useState('');
+    const [selectedOption, setSelectedOption] = useState('');
+    const [imagePreview, setImagePreview] = useState(''); // 추가된 부분
+    const selectFile = useRef("");
+
+    const handleSelectChange = (event) => {
+        setSelectedOption(event.target.value);
+        // "직접 입력"이 선택되면 customEmail 값을 초기화
+        if (event.target.value === "직접 입력") {
+            setCustomEmail('');
+        }
+    };
+
+    const handleCustomEmailChange = (event) => {
+        setCustomEmail(event.target.value);
+    };
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        // 이미지 미리보기를 위한 작업
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreview(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setImagePreview(null);
+        }
+    };
+
+    
 
     return (
         <>
-        <div className={style.mainContainer}>
+            <div className={style.mainContainer}>
+                <Note></Note>
+                <div className={style.signUpTitle}>회원가입</div>
 
-            <Note>
+                <div className={style.memberInfo}>
+
+                    <div>
+                        이름 : <input type="text" className={style.memberName} />
+                    </div>
+
+                    <div className={style.memberGender}>
+                        성별 :
+                        <label style={{marginLeft: "10px"}}>
+                            남 <input type="radio" value="male" checked={gender === 'female'} onChange={() => setGender('female')} />
+                        </label>
+                        <label>
+                            여 <input type="radio" value="female" checked={gender === 'male'} onChange={() => setGender('male')} />
+                        </label>
+                    </div>
+
+                    <div>
+                        나이 : <input type="text" className={style.memberAge} />
+                    </div>
+
+                    <div >
+                        이메일 : 
+
+                        <input type="text" className={style.memberEmail} />
+                        {selectedOption !== "직접 입력" && (
+                            <>
+                                @
+                                <select value={selectedOption} onChange={handleSelectChange}  className={style.memberEmail}>
+                                    <option value="" disabled>이메일을 선택해주세요.</option>
+                                    <option value="gmail.com">gmail.com</option>
+                                    <option value="naver.com">naver.com</option>
+                                    <option value="daum.net">daum.net</option>
+                                    <option value="outlook.com">outlook.com</option>
+                                    <option value="icloud.com">icloud.com</option>
+                                    <option value="직접 입력">직접 입력</option>
+                                </select>
+                            </>
+                        )}
+                        {selectedOption === "직접 입력" && (
+                            <>
+                                @
+                                <input 
+                                    type="text" 
+                                    value={customEmail} 
+                                    onChange={handleCustomEmailChange} 
+                                    placeholder="직접 입력"
+                                    className={style.memberEmail}
+                                />
+                            </>
+                        )}
+                    </div>
+
+                    <div className={style.IdContainer}>
+                        아이디 : <input type="text" className={style.memberId}/>
+                        <button className={style.iDuplcationButton}>중복확인</button>
+                    </div>
+
+
+                    <div className='wrapper'>
+                        비밀 번호 : <input type="password" className={style.memberPwd}/>
+                    </div>
+                    <div className='wrapper'>
+                        비밀 번호 확인 : <input type="password" className={style.memberPwd}/>
+                    </div>
+                    <div style={{color:"gray" , fontSize:"15px"}} className='wrapper'>
+                        비밀 번호 입력시 특수문자 !@#$%^&* 하나 이상이 포함되어야 합니다.
+                    </div>
+                    
+                    <div className='wrapper'>
+                        <div>
+                            전화번호 : <input type="text" className={style.memberPhone}/>
+                            
+                            <button className={style.checkButton}>인증번호 발송</button>
+                        
+                        </div>
+
+                        <div style={{color: "gray" , fontSize: "15px"}}> - 없이 입력해 주세요.</div>
+
+                        
+                    </div>
+
+                    <div>
+                        카카오톡 알림 인증번호 : <input type="text" className={style.authentication}/>
+
+                        <button className={style.checkButton}>확인하기</button>
+                    </div>
+                </div>
+
+
+                <div className={style.memberImg}>
+
+                        <div>
+                            
+                            <div className={style.changeImgTitle}>이미지 변환하기</div>
+
+                        </div>
+
+                        <div className={style.memberImgBefore}>
+                            <input type="file" onChange={handleImageChange} className={style.imgInput} ref={selectFile}/>
+                            
+                            <img src={imagePreview || baseImg} alt={baseImg} className={style.previewImage} />
+                            
+                            <button onclick={() => selectFile.current.onclick()} className={style.selectButton}>이미지 변환</button>
+
+                            <img src={downArrow} alt="" className={style.downArrowImg} />
+                            
+                        </div>
+
+
+                        <div className={style.memberImgAfter}>
+
+                            <img src={imagePreview || baseImg} alt={baseImg} className={style.previewImage} />
+
+                        </div>
+
+                </div>
+
+                <div className={style.successButtonContainer}>
+
+                    <button className={style.successButton}>완료</button>
+                </div>
                 
-            </Note>
-
-            <div className={style.signUpTitle}>회원가입</div>
-
-            <div className={style.memberInfo}>
-                <div>이름</div>
             </div>
-
-
-        </div>
-
-
         </>
     )
-
 }
 
-export default SignUp
+export default SignUp;
